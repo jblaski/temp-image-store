cd1001c19d565248038eebd03d32500178f73208
Julian Blaskiewicz <julian.blaskiewicz@outlook.com>
2021-05-20 19:57:28 +0100

    initial commit

---
+++ src/main/java/com/julianblaskiewicz/tempimagestore/TempImageStoreApplication.java
@@ -0,0 +1,13 @@
+package com.julianblaskiewicz.tempimagestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempImageStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempImageStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianblaskiewicz/tempimagestore/TempImageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianblaskiewicz.tempimagestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempImageStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
