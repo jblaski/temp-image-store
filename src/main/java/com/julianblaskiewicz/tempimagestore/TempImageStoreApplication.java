c9212e7f9d9b1f64d9c13a1e8fdc5c70467ffc5f
Julian Blaskiewicz <julian.blaskiewicz@outlook.com>
2021-05-20 20:23:28 +0100

    spring guides example

--- src/main/java/com/julianblaskiewicz/tempimagestore/TempImageStoreApplication.java
+++ src/main/java/com/julianblaskiewicz/tempimagestore/TempImageStoreApplication.java
@@ -1,13 +1,24 @@
 package com.julianblaskiewicz.tempimagestore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class TempImageStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempImageStoreApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
